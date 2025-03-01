b1feb98e65ee02d93f12dbd6d91c3a99ec960186
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-03-01 10:33:36 +0700

    refactor LandingPage layout and integrate Akomodasi component

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,51 +1,21 @@
 import HotelSearchSection from "../../components/Hero";
 import Banner from "../../assets/banner.webp";
 import KuponPengguna from "./components/KuponPengguna";
+import Akomodasi from "./components/Akomodasi";
 
 export default function LandingPage() {
   return (
     <>
       <HotelSearchSection />
-      <div className="max-w-screen-lg mx-auto mt-[360px] py-4">
+      <div className="max-w-[950px] mx-auto mt-[380px] py-4">
         <img
           src={Banner}
           alt="banner"
           className="w-full h-full object-cover rounded-lg"
         />
       </div>
-      <div className="max-w-screen-lg mx-auto py-4">
-        <h2 className="text-2xl font-semibold">Hotel Populer</h2>
-        <div className="grid grid-cols-3 gap-4 mt-6">
-          <div className="bg-gray-100 rounded-lg p-4">
-            <img
-              src="https://shadcnblocks.com/images/block/placeholder-dark-1.svg"
-              alt="hotel"
-              className="w-full h-48 object-cover rounded-lg"
-            />
-            <h3 className="text-lg font-semibold mt-4">Hotel Santika</h3>
-            <p className="text-gray-500 mt-2">Jl. Jendral Sudirman No. 49</p>
-          </div>
-          <div className="bg-gray-100 rounded-lg p-4">
-            <img
-              src="https://images.unsplash.com/photo-1587613861917-1e1b0f5e0e7e"
-              alt="hotel"
-              className="w-full h-48 object-cover rounded-lg"
-            />
-            <h3 className="text-lg font-semibold mt-4">Hotel Santika</h3>
-            <p className="text-gray-500 mt-2">Jl. Jendral Sudirman No. 49</p>
-          </div>
-          <div className="bg-gray-100 rounded-lg p-4">
-            <img
-              src="https://images.unsplash.com/photo-1587613861917-1e1b0f5e0e7e"
-              alt="hotel"
-              className="w-full h-48 object-cover rounded-lg"
-            />
-            <h3 className="text-lg font-semibold mt-4">Hotel Santika</h3>
-            <p className="text-gray-500 mt-2">Jl. Jendral Sudirman No. 49</p>
-          </div>
-        </div>
-      </div>
       <KuponPengguna />
+      <Akomodasi />
     </>
   );
 }
