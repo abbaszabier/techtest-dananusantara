71bdf453e0a8bf0d31b4757949c9101333337ee5
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-03-01 10:32:52 +0700

    refactor Hero component layout and integrate HotelSearch component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,71 +1,55 @@
 import { motion } from "framer-motion";
-import {
-  Calendar,
-  ChevronLeft,
-  ChevronRight,
-  CircleDollarSign,
-  History,
-  MapPin,
-  Moon,
-  Search,
-  User,
-} from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import Promo1 from "../assets/promo1.png";
 import Promo2 from "../assets/promo2.png";
+import HotelSearch from "./HotelSearch";
 
 const HotelSearchSection = () => {
   return (
-    <div className="relative bg-gradient-to-t from-[#3A7FCC] to-[#64B1E3] h-[350px] flex flex-col items-center justify-top">
-      <div className="flex flex-col md:flex-row items-top max-w-screen-xl px-20">
-        {/* Text Section */}
-
+    <div className="relative bg-linear-to-t from-[#3A7FCC] to-[#64B1E3] h-[320px] flex flex-col items-center justify-top">
+      <div className="grid grid-cols-1 lg:grid-cols-2 pl-3 w-full">
         <motion.div
           initial={{ opacity: 1 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.5 }}
-          className="text-white text-left flex flex-col justify-center px-8"
+          className="text-white text-left flex flex-col justify-center px-6 lg:pl-48 lg:pr-16"
         >
-          <h1 className="text-2xl font-semibold">
+          <h1 className="text-xl sm:text-2xl font-semibold">
             Booking Hotel & Penginapan Murah dengan Harga Promo
           </h1>
-          <p className="mt-4 text-lg md:text-xl">
+          <p className="mt-4 text-base sm:text-lg md:text-xl text-white/70">
             Temukan harga terbaik untuk setiap produk Traveloka yang Anda
             butuhkan.
           </p>
         </motion.div>
 
-        {/* Carousel Section */}
         <motion.div
           initial={{ x: "100%" }}
-          animate={{ x: "10%" }}
+          animate={{ x: "0%" }}
           transition={{ duration: 0.5 }}
-          className={`relative w-full`}
+          className="relative w-full overflow-hidden"
         >
-          <div className="flex gap-x-4 p-2 scrollbar-hide">
-            {
-              <img
-                src={Promo1}
-                alt="promo1"
-                className="h-[250px] w-[500px] aspect-[4/2] rounded-lg object-cover shadow-lg"
-              />
-            }
-            {
-              <img
-                src={Promo2}
-                alt="promo2"
-                className="h-[250px] w-[500px] aspect-[4/2] rounded-lg object-cover shadow-lg"
-              />
-            }
+          <div className="hidden lg:flex flex-col sm:flex-row gap-4 py-2 w-full">
+            <img
+              src={Promo1}
+              alt="promo1"
+              className="h-[180px] sm:h-[230px] aspect-[4/2] rounded-lg object-cover"
+            />
+            <img
+              src={Promo2}
+              alt="promo2"
+              className="h-[180px] sm:h-[230px] w-full sm:w-[500px] aspect-[4/2] rounded-lg object-cover"
+            />
           </div>
         </motion.div>
       </div>
 
-      <div className="absolute w-[200px] top-[280px] transform -translate-y-1/2 flex w-full justify-center px-4">
+      <div className="absolute w-[200px] top-[255px] transform -translate-y-1/2 flex w-full justify-center">
         <button>
           <ChevronLeft size={20} className="text-white/30" />
         </button>
-        <p className="mx-[100px] text-sm text-white  text-center flex flex-col justify-center">
+        <p className="mx-[110px] text-[12px] text-white/90  text-center flex flex-col font-semibold justify-center">
           Lihat promo lainnya
         </p>
         <button>
@@ -73,7 +57,7 @@ const HotelSearchSection = () => {
         </button>
       </div>
 
-      <div className="absolute top-[300px] left-1/2 transform -translate-x-1/2 w-full max-w-screen-lg bg-white rounded-lg shadow-lg p-4">
+      {/* <div className="absolute top-[300px] left-1/2 transform -translate-x-1/2 w-full max-w-screen-lg bg-white rounded-lg shadow-lg p-4">
         <div className="mb-4 flex text-sm items-center text-blue-500 font-semibold">
           <History className="mr-2" /> Hotel yang Terakhir Dilihat
         </div>
@@ -156,7 +140,8 @@ const HotelSearchSection = () => {
             Bayar di Hotel
           </button>
         </div>
-      </div>
+      </div> */}
+      <HotelSearch />
     </div>
   );
 };
