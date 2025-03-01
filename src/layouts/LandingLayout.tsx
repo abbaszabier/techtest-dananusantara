d587bcf374f6f82703849f461641ff3da45f0c70
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-03-01 10:33:17 +0700

    add Footer component to LandingLayout and improve help button styling

--- src/layouts/LandingLayout.tsx
+++ src/layouts/LandingLayout.tsx
@@ -1,18 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { CircleHelp } from "lucide-react";
+import Footer from "../components/Footer";
 
 export default function MainLayout() {
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
       <Outlet />
-      <div className="fixed right-6 bottom-6">
-        <button className="flex items-center gap-2 bg-blue-500 text-white px-3 py-2 rounded-full">
-          <CircleHelp className="text-white" />
-          <span className="text-sm">Perlu Bantuan?</span>
+      <div className="fixed right-4 bottom-4">
+        <button className="flex items-center gap-1 bg-[#0194f3] text-white px-3 py-2 shadow rounded-full cursor-pointer hover:bg-[#0077c2]">
+          <CircleHelp className="text-white" fontVariant="bold" size={22} />
+          <span className="text-base font-bold">Perlu Bantuan?</span>
         </button>
       </div>
+      <Footer />
     </div>
   );
 }
